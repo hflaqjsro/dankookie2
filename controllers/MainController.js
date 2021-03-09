a65da21a7cb44604c1afe6096a5ba92fc5d69469
hflaqjsro <76904429+hflaqjsro@users.noreply.github.com>
2021-03-09 21:25:12 +0900

    Add files via upload

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -7,10 +7,6 @@ export default {
     init() {
         console.log(tag, 'init()');
         MainView.setup(document.querySelector('.start'));
-        if(document.querySelector('.start').style.display == 'none'){
-            QuestionView.setup(document.querySelector('.question'))
-        } else {
-            
-        }
+
     }
 }
