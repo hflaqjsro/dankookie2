24f0d4b43a66c8d1e6db44a426137c5d395dfae4
hflaqjsro <76904429+hflaqjsro@users.noreply.github.com>
2021-03-13 16:11:19 +0900

    Add files via upload

--- views/QuestionView.js
+++ views/QuestionView.js
@@ -1,3 +1,4 @@
+import MainController from '../controllers/MainController.js';
 import View from './View.js'
 
 const tag = '[QuestionView]';
@@ -6,51 +7,42 @@ const QuestionView = Object.create(View);
 
 QuestionView.setup = function (el) {
     this.init(el);
+    this.ABtn = document.querySelector("#A");
+    this.BBtn = document.querySelector("#B");
     this.showQuestion();
 }
 
 QuestionView.showQuestion = function (el) {
     this.show();
-    next();
+    this.ABtn.addEventListener('click', e => this.AonClick(e));
+    this.BBtn.addEventListener('click', e => this.BonClick(e));
+    //MainController.next();
 }
 
+QuestionView.AonClick = function() {
+    console.log(tag, 'AonClick');
+    this.type = document.querySelector('#type').getAttribute('value');
+    this.preValue = document.querySelector("#"+this.type).getAttribute('value');
+    MainController.preValuePlus();
+    MainController.next();
+}
+
+QuestionView.BonClick = function() {
+    console.log(tag, 'BonClick');
+    MainController.next();
+}
+
+/*
 $("#A").click(function(){
     var type = $("#type").val();
     var preValue = $("#"+type).val();
     $("#"+type).val(parseInt(preValue)+1);
     next();
 });
-document.querySelector("#B").click()
 
 $("#B").click(function(){
     next();
 });
-function next () {
-    var num = 1;
-    if(num = 13){                
-        $(".question").hide();
-        $(".result").show();
-        var mbti = "";
-        ($("#EI").val() < 2) ? mbti+="I" : mbti+="E";
-        ($("#SN").val() < 2) ? mbti+="N" : mbti+="S";
-        ($("#TF").val() < 2) ? mbti+="F" : mbti+="T";
-        ($("#JP").val() < 2) ? mbti+="P" : mbti+="J";
-    }else {
-        $(".progress-bar").attr('style', 'width: calc(100/12*'+num+'%)');
-        $("#pro_num").html(q[num]["pro_num"]);
-        $("#title").html(q[num]["title"]);
-        $("#type").val(q[num]["type"])
-        $("#A").html(q[num]["A"]);
-        $("#B").html(q[num]["B"]);
-        num++;
-    }
-}
-$("#Y").click(function(){
-    $(".result").hide();
-    $(".result02").show();
- });
- $("#X").click(function(){
-    $(".result02").hide();
-    $(".result").show();
- });
+*/
+
 export default QuestionView;
