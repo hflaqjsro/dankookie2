d590f544c0b155f9c53902bc51a67b35cd244144
hflaqjsro <76904429+hflaqjsro@users.noreply.github.com>
2021-03-31 18:39:27 +0900

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,21 @@
+const start_img = document.querySelector('.start_img');
+
+
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src = '/img/0'+(imgNumber)+'.png';
+    image.classList.add("start_png");
+    start_img.appendChild(image);
+}
+
+function genRandom(){
+    const number = Math.floor(Math.random() * 10);
+    return number;
+}
+
+function init(){
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+
+init();
