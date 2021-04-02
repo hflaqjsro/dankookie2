abf3497c11f9d8793070e95d9b9c1839a682f7cc
hflaqjsro <76904429+hflaqjsro@users.noreply.github.com>
2021-04-02 16:34:58 +0900

    Add files via upload

---
+++ QuestionView.js
@@ -0,0 +1,48 @@
+import MainController from "../controllers/MainController.js";
+import View from "./View.js";
+
+const tag = "[QuestionView]";
+
+const QuestionView = Object.create(View);
+
+QuestionView.setup = function (el) {
+  this.init(el);
+  this.ABtn = document.querySelector("#A");
+  this.BBtn = document.querySelector("#B");
+  this.showQuestion();
+};
+
+QuestionView.showQuestion = function (el) {
+  this.show();
+  this.ABtn.addEventListener("click", (e) => this.AonClick(e));
+  this.BBtn.addEventListener("click", (e) => this.BonClick(e));
+  this.num = 1;
+  MainController.next();
+};
+
+QuestionView.AonClick = function () {
+  console.log(tag, "AonClick");
+  this.type = document.querySelector("#type").getAttribute("value");
+  MainController.preValuePlus(this.type);
+  MainController.next();
+};
+
+QuestionView.BonClick = function () {
+  console.log(tag, "BonClick");
+  MainController.next();
+};
+
+/*
+$("#A").click(function(){
+    var type = $("#type").val();
+    var preValue = $("#"+type).val();
+    $("#"+type).val(parseInt(preValue)+1);
+    next();
+});
+
+$("#B").click(function(){
+    next();
+});
+*/
+
+export default QuestionView;
